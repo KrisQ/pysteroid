f4ebeeeca528c85835389fcc2a7c3537ec11d9b2
KrisQ <kquemener1@gmail.com>
2025-07-01 21:49:29 -0500

    feat: base game

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pygame 
+import sys
+
 from constants import  SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print("Starting Asteroids!")
@@ -7,12 +13,51 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
+    # Set Player.containers as a class variable (not instance variable)
+    # This tells pygame: "Every Player object created should automatically 
+    # be added to these sprite groups." Class variables are shared by ALL
+    # instances of the Player class, so this setting applies to every player.
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable, shots)
+
+    # We still need to create actual instances! Setting .containers only tells
+    # pygame WHERE to put objects when they're created - it doesn't create them.
+    # Think of containers as "registration instructions" that say "when you make
+    # a Player, automatically add it to these groups." But we still have to
+    # actually make the Player and AsteroidField objects for the game to work.
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for a in asteroids:
+            if a.check_collision(player):
+                print("Game over!")
+                sys.exit()
+            for s in shots:
+                if a.check_collision(s):
+                    a.split()
+                    s.kill()
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
+        
+        # limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,58 @@
+import pygame
+from circleshape import CircleShape
+from shot import Shot
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+        self.timer = 0
+
+    def shoot(self):
+        if not self.timer > 0:
+            shot = Shot(self.position[0], self.position[1])
+            forward = pygame.Vector2(0, 1).rotate(self.rotation)
+            shot.velocity += forward * PLAYER_SHOOT_SPEED
+            self.timer = PLAYER_SHOOT_COOLDOWN
+
+    
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+    
+    def rotate(self, dt):
+        self.rotation += dt * PLAYER_TURN_SPEED
+
+    def move(self, dt):
+        # Create a unit vector pointing "up" (0, 1) in screen coordinates
+        # Then rotate it by the player's current rotation angle
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        
+        # Move the player's position in the forward direction
+        # Multiply by PLAYER_SPEED for movement rate and dt for frame-rate independence
+        self.position += forward * PLAYER_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        self.timer -= dt
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+        
